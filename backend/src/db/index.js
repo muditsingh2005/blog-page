3193d318ae4bace871288df1a46f9a5e52061e8f
Mudit Singh <muditsingh6624@gmail.com>
2025-07-13 21:21:49 +0530

    error and response  handling

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -6,9 +6,11 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\n MongoDB connected !! DB Host: ${connectionInstance}`);
+    console.log(
+      `\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
-    console.log("MongoDB connection error: ", error);
+    console.log("MongoDB connection error : ", error);
     process.exit(1);
   }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
@@ -8,8 +8,12 @@ dotenv.config({
   path: "../.env",
 });
 
-connectDB();
-
-// app.listen(process.env.PORT || 8000, () => {
-//   console.log(`Server is running at port : ${process.env.PORT}`);
-// });
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection failed !! ", error);
+  });
