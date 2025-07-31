1a413eb088c32775723ed496fa4dfa344686d406
Mudit Singh <muditsingh6624@gmail.com>
2025-07-31 23:43:42 +0530

    error fixed in user reg

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -23,15 +23,6 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
 
-  let coverImageLocalPath;
-  if (
-    req.files &&
-    Array.isArray(req.files.coverImage) &&
-    req.files.coverImage.length > 0
-  ) {
-    coverImageLocalPath = req.files.coverImage[0].path;
-  }
-
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
@@ -39,7 +30,6 @@ const registerUser = asyncHandler(async (req, res) => {
   //upload on cloudinary
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
-  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
     throw new ApiError(400, "Avatar file is required");
@@ -50,7 +40,6 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     fullName,
     avatar: avatar.url,
-    coverImage: coverImage?.url || "",
     email,
     password,
     username: username.toLowerCase(),
