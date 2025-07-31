15a1aed39fa71a089ee15b5ef98d2db5661befe6
Mudit Singh <muditsingh6624@gmail.com>
2025-07-31 23:40:25 +0530

    add user registration functionality

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,74 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
+
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() == "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existerUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existerUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  //upload on cloudinary
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  //now create a object and send it to db
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  // console.log(res); for research purpose
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+});
+
+export { registerUser };
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,6 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-// const app = express();
-
 dotenv.config({
   path: "../.env",
 });
