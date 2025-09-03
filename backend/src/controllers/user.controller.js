ece75e617866b7a6db73fce603f30bcd2859284c
Mudit Singh <muditsingh6624@gmail.com>
2025-09-03 22:29:45 +0530

    add change password and current user routes; implement password change logic and user retrieval

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -218,4 +218,40 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 }); //declare route to do so..
 
-export { registerUser, loginUser, logoutUser };
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  //if user is able to change password, means s=user is logged in ,means through auth.middleware user access is given in this controller
+
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid old password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user?._id).select(
+    "-password -refreshToken"
+  );
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Current user fetched successfully"));
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  refreshAccessToken,
+};
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,6 +3,9 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -22,5 +25,10 @@ router.route("/login").post(loginUser);
 
 //secured route
 router.route("/logout").post(verifyJWT, logoutUser);
+
+//testing for these routes before push on git...
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/refresh-access-token").post(refreshAccessToken);
 
 export default router;
